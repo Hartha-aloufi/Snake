7aa53e658d00cf801f90edfe19a22d6d1234689b
Hartha-aloufi <Hartha.programmer@gmail.com>
2016-04-30 22:51:50 +0300

    main functionality

---
+++ index.js
@@ -0,0 +1,9 @@
+var app = require('express')();
+var server = require('http').createServer(app);
+
+server.listen(3000);
+console.log('server is now running...');
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
